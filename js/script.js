1c719fe96cfd381c61f0bfcd23aa4c7bff177d8d
jessica engel & Drew <drew@finstrom.us>
2015-05-27 09:47:50 -0700

    add function to add 'ay' in cases where leading letter is AEIO or U

--- js/script.js
+++ js/script.js
@@ -12,3 +12,13 @@ $( document ).ready(function() {
 var foo = function(bar) {
   return false;
 };
+
+var add_ay = function(text) {
+  var regex = new RegExp("[aeiouAEIOU]");
+  var temp = text.substring(0, 1).match(regex);
+  if (temp !== null && temp.length === 1) {
+    return text = text + "ay";
+  } else {
+    return text;
+  }
+}
--- spec/specs.js
+++ spec/specs.js
@@ -3,3 +3,11 @@ describe('foo', function() {
     expect(foo('bar')).to.equal(false);
   });
 });
+
+//pig_latin
+describe('add_ay', function() {
+  it("appends 'ay' to word starting with a vowel", function() {
+
+    expect(add_ay('add')).to.equal('adday');
+  });
+});
